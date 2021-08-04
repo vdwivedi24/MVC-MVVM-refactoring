2615778039a58da943ba655691d2e9a1a6a82cd6
vdwivedi24 <vkrntdwivedi224@gmail.com>
2021-08-04 13:10:41 -0400

    Added the MVC code to view controller file as well for reference

--- MVVMFromMVC/Controllers/WeatherViewController.swift
+++ MVVMFromMVC/Controllers/WeatherViewController.swift
@@ -1,4 +1,69 @@
 
+ // MVC Code:
+ 
+ /*
+ import UIKit
+
+ class WeatherViewController: UIViewController {
+   private let geocoder = LocationGeocoder()
+   private let defaultAddress = "McGaheysville, VA"
+   private let dateFormatter: DateFormatter = {
+     let dateFormatter = DateFormatter()
+     dateFormatter.dateFormat = "EEEE, MMM d"
+     return dateFormatter
+   }()
+   private let tempFormatter: NumberFormatter = {
+     let tempFormatter = NumberFormatter()
+     tempFormatter.numberStyle = .none
+     return tempFormatter
+   }()
+   
+   @IBOutlet weak var cityLabel: UILabel!
+   @IBOutlet weak var dateLabel: UILabel!
+   @IBOutlet weak var currentIcon: UIImageView!
+   @IBOutlet weak var currentSummaryLabel: UILabel!
+   @IBOutlet weak var forecastSummary: UITextView!
+   
+   override func viewDidLoad() {
+     geocoder.geocode(addressString: defaultAddress) { [weak self] locations in
+       guard
+         let self = self,
+         let location = locations.first
+         else {
+           return
+         }
+       self.cityLabel.text = location.name
+       self.fetchWeatherForLocation(location)
+     }
+   }
+
+   func fetchWeatherForLocation(_ location: Location) {
+     //1
+     WeatherbitService.weatherDataForLocation(
+       latitude: location.latitude,
+       longitude: location.longitude) { [weak self] (weatherData, error) in
+       //2
+       guard
+         let self = self,
+         let weatherData = weatherData
+         else {
+           return
+         }
+       self.dateLabel.text =
+         self.dateFormatter.string(from: weatherData.date)
+       self.currentIcon.image = UIImage(named: weatherData.iconName)
+       let temp = self.tempFormatter.string(
+         from: weatherData.currentTemp as NSNumber) ?? ""
+       self.currentSummaryLabel.text =
+         "\(weatherData.description) - \(temp)℉"
+       self.forecastSummary.text = "\nSummary: \(weatherData.description)"
+     }
+   }
+ }
+*/
+
+// Helper: MVVM code
+/*
 import UIKit
 
 class WeatherViewController: UIViewController {
@@ -11,22 +76,19 @@ class WeatherViewController: UIViewController {
   private let viewModel = WeatherViewModel()
   
   override func viewDidLoad() {
+
     viewModel.locationName.bind { [weak self] locationName in
       self?.cityLabel.text = locationName
     }
-    
     viewModel.date.bind { [weak self] date in
       self?.dateLabel.text = date
     }
-    
     viewModel.icon.bind { [weak self] image in
       self?.currentIcon.image = image
     }
-    
     viewModel.summary.bind { [weak self] summary in
       self?.currentSummaryLabel.text = summary
     }
-    
     viewModel.forecastSummary.bind { [weak self] forecast in
       self?.forecastSummary.text = forecast
     }
@@ -50,3 +112,5 @@ class WeatherViewController: UIViewController {
     present(alert, animated: true)
   }
 }
+ 
+ */
